8ba1b34725a88ebc741742f25b4034fe30c812df
clem <clemmarcella.edu.ph>
2026-04-30 17:56:06 +0800

    dbrename

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","root","","seniorcare_new");
+$conn=mysqli_connect("localhost","root","","seniorcare_newdb");
 if(!$conn){
 	("Connection Failed:" . mysqli_connect_error());
 }
