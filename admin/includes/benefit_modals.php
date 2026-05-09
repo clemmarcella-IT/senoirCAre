2ef09700d2e0cd40b0e915fd3c3c4d92ec71fb1e
clem <clemmarcella.edu.ph>
2026-05-09 18:30:45 +0800

    modal and notif

--- admin/includes/benefit_modals.php
+++ admin/includes/benefit_modals.php
@@ -30,3 +30,27 @@
         </div>
     </div>
 </div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal<?php echo $row['LogID']; ?>" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content border-0 shadow-lg">
+            <div class="modal-header bg-danger text-white">
+                <h5 class="modal-title fw-bold">Delete Benefit Claim</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center p-4">
+                <i class="fa fa-exclamation-triangle fa-3x text-danger mb-3"></i>
+                <p class="fw-bold">Are you sure you want to delete this benefit claim?</p>
+                <h5 class="text-dark"><?php echo $name; ?> - ₱<?php echo number_format($row['Amount_Released'], 2); ?></h5>
+            </div>
+            <div class="modal-footer justify-content-center border-0">
+                <button type="button" class="btn btn-secondary px-4" data-bs-dismiss="modal">Cancel</button>
+                <form action="query_benefits_crud.php" method="POST" class="m-0 p-0">
+                    <input type="hidden" name="log_id" value="<?php echo $row['LogID']; ?>">
+                    <button type="submit" name="delete_claim" class="btn btn-danger px-4 fw-bold">Yes, Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
